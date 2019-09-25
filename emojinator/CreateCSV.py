f95c5f9c6f854e009ecf929683757b605860040a
hatake-kakashi2202 <44166657+hatake-kakashi2202@users.noreply.github.com>
2019-09-25 16:29:54 +0530

    Add files via upload

---
+++ emojinator/CreateCSV.py
@@ -0,0 +1,15 @@
+from scipy.misc import imread
+import numpy as np
+import pandas as pd
+import os
+root = './gestures'
+
+for directory,subdirectories,files in os.walk(root):
+	for file in files:
+		print(file)
+		im = imread(os.path.join(directory,file))
+		value =im.flatten()
+		value =np.hstack((directory[11:],value))
+		df = pd.DataFrame(value).T
+		with open('train.csv','a') as dataset:
+			df.to_csv(dataset,header=False,index=False)
